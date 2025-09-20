7150669ffc09565f46e6e498db8d1a6f97f7af4b
qinjunhaocn <avf_j7b6@163.com>
2025-09-20 23:21:39 +0800

    push

--- budget_simple/lib/main.dart
+++ budget_simple/lib/main.dart
@@ -222,4 +222,10 @@ class ScrollBehavior extends MaterialScrollBehavior {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
       };
+      
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    // 禁用所有滚动效果
+    return const NeverScrollableScrollPhysics();
+  }
 }
